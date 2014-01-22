c0e286ff6db25630eb02e1ae4ede23439d12167e
Mark Duerden <markduerden@gmail.com>
2014-01-22 15:40:28 +0000

    beyond local responsive v1

--- js/homepage_validation.js
+++ js/homepage_validation.js
@@ -35,11 +35,6 @@ $(document).ready(function(){
 			highlight: function(element) {
 				$(element).closest('.control-group').removeClass('success').addClass('error');
 			},
-			success: function(element) {
-				element
-				.text('OK!').addClass('valid')
-				.closest('.control-group').removeClass('error').addClass('success');
-			}
 	  });
 
 }); // end document.ready
